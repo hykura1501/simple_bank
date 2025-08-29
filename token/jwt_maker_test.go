80900698048bedff0c4c808560a1f6e02780e95a
phiho1501 <ho.vp@kit-inc.jp>
2025-08-30 00:11:19 +0700

    #20 How to create and verify JWT & PASETO token in Golang

---
+++ token/jwt_maker_test.go
@@ -0,0 +1,70 @@
+package token
+
+import (
+	"testing"
+	"time"
+
+	"github.com/golang-jwt/jwt"
+	"github.com/hykura1501/simple_bank/util"
+	"github.com/stretchr/testify/require"
+)
+
+func TestJWTMaker(t *testing.T) {
+	secretKey := util.RandomString(32)
+	maker, err := NewJWTMaker(secretKey)
+
+	require.NoError(t, err)
+	username := util.RandomOwner()
+	duration := time.Second
+	issuedAt := time.Now()
+	expiredAt := issuedAt.Add(duration)
+
+	token, err := maker.CreateToken(username, duration)
+	require.NoError(t, err)
+	require.NotEmpty(t, token)
+
+	payload, err := maker.VerifyToken(token)
+	require.NoError(t, err)
+	require.NotEmpty(t, payload)
+
+	require.NotZero(t, payload.ID)
+	require.Equal(t, username, payload.Username)
+	require.WithinDuration(t, issuedAt, payload.IssuedAt, time.Second)
+	require.WithinDuration(t, expiredAt, payload.ExpiredAt, time.Second)
+}
+
+func TestExpiredJWTToken(t *testing.T) {
+	secretKey := util.RandomString(32)
+	maker, err := NewJWTMaker(secretKey)
+	require.NoError(t, err)
+
+	token, err := maker.CreateToken(util.RandomOwner(), -time.Minute)
+	require.NoError(t, err)
+	require.NotEmpty(t, token)
+
+	payload, err := maker.VerifyToken(token)
+	require.Error(t, err)
+	require.EqualError(t, err, ErrExpiredToken.Error())
+	require.Nil(t, payload)
+}
+
+func TestInvalidJWTTokenAlgNone(t *testing.T) {
+	payload, err := NewPayload(util.RandomOwner(), time.Minute)
+	require.NoError(t, err)
+	require.NotEmpty(t, payload)
+
+	jwtToken := jwt.NewWithClaims(jwt.SigningMethodNone, payload)
+	require.NotEmpty(t, jwtToken)
+	token, err := jwtToken.SignedString(jwt.UnsafeAllowNoneSignatureType)
+	require.NoError(t, err)
+	require.NotEmpty(t, token)
+
+	secretKey := util.RandomString(32)
+	maker, err := NewJWTMaker(secretKey)
+	require.NoError(t, err)
+
+	payload, err = maker.VerifyToken(token)
+	require.Error(t, err)
+	require.EqualError(t, err, ErrInvalidToken.Error())
+	require.Nil(t, payload)
+}
